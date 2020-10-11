185d8672554c6d1e83ce0ae7c73fb8dcc3220ab9
madhujella <r.madhuj68@gmail.com>
2020-10-11 19:14:21 +0530

    Updates

---
+++ precache-manifest.e3dfb80389265cab7da9079c3111a762.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417b2d4af652b837f7a166453245b571",
+    "url": "/scalenut-task/index.html"
+  },
+  {
+    "revision": "394d7d6df26ca7c79d48",
+    "url": "/scalenut-task/static/css/2.d2fc255c.chunk.css"
+  },
+  {
+    "revision": "22f110d48f9a429bb574",
+    "url": "/scalenut-task/static/css/main.e74facf6.chunk.css"
+  },
+  {
+    "revision": "394d7d6df26ca7c79d48",
+    "url": "/scalenut-task/static/js/2.1442d5e9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/scalenut-task/static/js/2.1442d5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f110d48f9a429bb574",
+    "url": "/scalenut-task/static/js/main.5212414d.chunk.js"
+  },
+  {
+    "revision": "08ccabd91d8e26958f10",
+    "url": "/scalenut-task/static/js/runtime-main.d0a22ae8.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/scalenut-task/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/scalenut-task/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/scalenut-task/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/scalenut-task/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/scalenut-task/static/media/primeicons.79432094.woff"
+  },
+  {
+    "revision": "ac5f028744a537a50b7a4245b8ec631b",
+    "url": "/scalenut-task/static/media/thumbnail.ac5f0287.jpg"
+  }
+]);
